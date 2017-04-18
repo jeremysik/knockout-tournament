69b4da4d450cef625e47329fa81eec5b8ff04b52
Jeremy Sik <jeremy.sik@me.com>
2017-04-18 16:59:44 +1000

    Partial implementation

--- client/client.js
+++ client/client.js
@@ -1,2 +1,11 @@
-// Edit me.
-// Feel free to add other JS files in this directory as you see fit.
+var tournamentService = new TournamentService();
+
+const startTournament = () => {
+	let numberOfTeams = document.getElementById('numberOfTeams').value,
+		teamsPerMatch = document.getElementById('teamsPerMatch').value;
+
+	if(numberOfTeams < 1 || teamsPerMatch < 1 || !Number.isInteger(numberOfTeams) || !Number.isInteger(teamsPerMatch)) {
+		alert('Please enter integers greater than 0');
+	}
+	console.log(numberOfTeams, teamsPerMatch);
+}
---
+++ client/tournamentService.js
@@ -0,0 +1,27 @@
+class TournamentService {
+
+	static endpoint() {
+		return "http://localhost:8765/";
+	}
+
+	createTournament(numberOfTeams, teamsPerMatch) {
+
+	}
+
+	getTeam(tournamentId, teamId) {
+
+	}
+
+	getMatch(tournamentId, round, match) {
+
+	}
+
+	getWinner(tournamentId, teamScores, matchScore) {
+
+	}
+
+	_call(method, data) {
+
+	}
+
+}
